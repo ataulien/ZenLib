3fc9c00ef889f69b1e0a26818d219f57826ab76f
Andre Taulien <stu114706@uni-kiel.de>
2016-12-30 19:32:02 +0100

    Added unaligned read

--- utils/alignment.h
+++ utils/alignment.h
@@ -2,9 +2,21 @@
 #include <inttypes.h>
 #include <string.h>
 
+#if EMSCRIPTEN
+#include <emscripten.h>
+#endif
+
 namespace Utils
 {
-
+#if EMSCRIPTEN
+    typedef emscripten_align1_int unaligned_int32;
+    typedef emscripten_align1_short unaligned_int16;
+    typedef emscripten_align1_float unaligned_float;
+#else
+    typedef int32_t unaligned_int32;
+    typedef int16_t unaligned_int16;
+    typedef float unaligned_float;
+#endif
 	/**
 	 * @brief performs an unaligned memory-access
 	 */
@@ -13,4 +25,12 @@ namespace Utils
 	{
 		memcpy(dst, src, sizeof(T));
 	}
+
+    template<typename T>
+    void unalignedRead(T& dst, const void*& src)
+    {
+        getUnaligned(&dst, src);
+        uint8_t*& ptr = ((uint8_t*&)src);
+        ptr += sizeof(T);
+    }
 }
