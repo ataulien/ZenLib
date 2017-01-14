f1d53ad14e161701417d3bc8a0642f91f30a00f9
Alexander Stillich <dr.west@zoho.com>
2017-01-14 16:19:33 +0100

    Restructured code so that multiple file types can be read

--- zenload/zCModelAni.h
+++ zenload/zCModelAni.h
@@ -1,106 +1,129 @@
-#pragma once 
+#pragma once
+#include <functional>
+#include <string>
 #include <vector>
 #include "utils/mathlib.h"
 #include "zTypes.h"
 
 namespace VDFS
 {
-	class FileIndex;
+    class FileIndex;
 }
 
 namespace ZenLoad
 {
-	class ZenParser;
-	class zCModelAni
-	{
-	public:
+    class ZenParser;
 
-		struct zCModelAniEvent
-		{
-			enum { ANIEVENT_MAXSTRING = 4 };
+    struct zCModelAniHeader
+    {
+        uint16_t version;
 
-			zTMdl_AniEventType aniEventType;
-			uint32_t frameNr;
-			std::string tagString;
-			std::string string[ANIEVENT_MAXSTRING];
-			float values[ANIEVENT_MAXSTRING];
-			float prob;
+        std::string aniName;
 
-			void load(ZenParser& parser);
-		};
+        uint32_t layer;
+        uint32_t numFrames;
+        uint32_t numNodes;
+        float fpsRate;
+        float fpsRateSource;
+        float samplePosRangeMin;
+        float samplePosScaler;
 
-		struct ModelAniHeader
-		{
-			uint16_t version;
+        ZMath::float3 aniBBox[2];
 
-			std::string aniName;
+        std::string nextAniName;
 
-			uint32_t layer;
-			uint32_t numFrames;
-			uint32_t numNodes;
-			float fpsRate;
-			float fpsRateSource;
-			float samplePosRangeMin;
-			float samplePosScaler;
+        uint32_t nodeChecksum;
+    };
 
-			ZMath::float3 aniBBox[2];
+    struct zCModelAniEvent
+    {
+        enum { ANIEVENT_MAXSTRING = 4 };
 
-			std::string nextAniName;
+        zTMdl_AniEventType aniEventType;
+        uint32_t frameNr;
+        std::string tagString;
+        std::string string[ANIEVENT_MAXSTRING];
+        float values[ANIEVENT_MAXSTRING];
+        float prob;
 
-			uint32_t nodeChecksum;
-		};
+        void load(ZenParser& parser);
+    };
 
-		struct AniSample
-		{
-			ZMath::float4 rotation; // Quaternion
-			ZMath::float3 position;
-		};
+    struct zCModelAniSample
+    {
+        ZMath::float4 rotation; // Quaternion
+        ZMath::float3 position;
+    };
 
-		zCModelAni(){}
+    class zCModelAni
+    {
+    public:
 
-		/**
-		* @brief Loads the mesh from the given VDF-Archive
-		*/
-		zCModelAni(const std::string& fileName, const VDFS::FileIndex& fileIndex, float scale = 1.0f);
+        zCModelAni();
 
-		/**
-		 * @brief Reads the mesh-object from the given binary stream
-		 * @param fromZen Whether this mesh is supposed to be read from a zenfile. In this case, information about the binary chunk is also read.
-		 */
-		void readObjectData(ZenParser& parser);
+        void setScale(float scale) { m_Scale = scale; }
 
-		/**
-		 * @return generic information about this animation
-		 */
-		const ModelAniHeader& getModelAniHeader() const { return m_ModelAniHeader; }
+        /** Loads a model animation file (.MAN).
+         *
+         * You need to call either loadMSB() and loadMDH() afterwards or
+         * call loadMDS() to load additional required data.
+         *
+         */
+        bool loadMAN(const VDFS::FileIndex& fileIndex, const std::string& fileName);
 
-		/**
-		 * @return Animation-data. Access: sampleIdx * numNodes + node
-		 */
-		const std::vector<AniSample>& getAniSamples() const { return m_AniSamples; }
+        /** Loads a model hierarchy (.MDH) file.
+         *
+         */
+        bool loadMDH(const std::string& fileName);
 
-		/**
-		 * @return Indices of the samples to the actual nodes
-		 */
-		const std::vector<uint32_t>& getNodeIndexList() const { return m_NodeIndexList; }
-		
-		/**
+        /** Loads a compiled MDS (.MSB) file.
+         *
+         */
+        bool loadMSB(const std::string& fileName);
+
+        /** Loads a model script (.MDS) file.
+         *
+         */
+        bool loadMDS(const std::string& fileName);
+
+        /**
+         * @return generic information about this animation
+         */
+        const zCModelAniHeader& getModelAniHeader() const { return m_ModelAniHeader; }
+
+        /**
+         * @return Animation-data. Access: sampleIdx * numNodes + node
+         */
+        const std::vector<zCModelAniSample>& getAniSamples() const { return m_AniSamples; }
+
+        /**
+         * @return Indices of the samples to the actual nodes
+         */
+        const std::vector<uint32_t>& getNodeIndexList() const { return m_NodeIndexList; }
+
+        /**
          * @return Whether the animation was correctly loaded
          */
         bool isValid(){ return m_ModelAniHeader.version != 0; }
 
-	private:
-		/**
-		 * @brief File information
-		 */
-		ModelAniHeader m_ModelAniHeader;
+    private:
 
-		/**
-		 * @brief Ani-Events
-		 */
-		std::vector<zCModelAniEvent> m_AniEvents;
+        float m_Scale = 1.0f;
 
-		std::vector<uint32_t> m_NodeIndexList;
-		std::vector<AniSample> m_AniSamples;
-	};
+        /**
+         * @brief File information
+         */
+        zCModelAniHeader m_ModelAniHeader;
+
+        /**
+         * @brief Ani-Events
+         */
+        std::vector<zCModelAniEvent> m_AniEvents;
+
+        std::vector<uint32_t> m_NodeIndexList;
+        std::vector<zCModelAniSample> m_AniSamples;
+
+
+        bool parse(const VDFS::FileIndex &fileIndex, const std::string &fileName, std::function<bool (ZenParser &)> fn);
+    };
 }
