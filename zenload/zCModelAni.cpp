7c89afb1264d108b29b5d773082cf04ac34a9843
Alexander Stillich <dr.west@zoho.com>
2017-01-16 21:36:31 +0100

    Added some MSB loading code (will get refactored)

--- zenload/zCModelAni.cpp
+++ zenload/zCModelAni.cpp
@@ -1,19 +1,54 @@
 #include <algorithm>
 #include <cmath>
+#include <iomanip>
 #include <string>
-#include "zCModelAni.h"
-#include "zenParser.h"
+
 #include "utils/logger.h"
-#include "zTypes.h"
 #include "vdfs/fileIndex.h"
+#include "zCModelAni.h"
 #include "zCProgMeshProto.h"
+#include "zenParser.h"
+#include "zTypes.h"
 
 using namespace ZenLoad;
 
+enum
+{
+    MSB_MODEL_SCRIPT     = 0xF000,
+    MSB_SOURCE           = 0xF100,
+    MSB_MODEL            = 0xF200,
+    MSB_MESH_AND_TREE    = 0xF300,
+    MSB_REGISTER_MESH    = 0xF400,
+    MSB_ANI_ENUM         = 0xF500,
+    MSB_ANI_MAX_FPS      = 0xF510,
+    MSB_ANI              = 0xF520,
+    MSB_ANI_ALIAS        = 0xF530,
+    MSB_ANI_BLEND        = 0xF540,
+    MSB_ANI_SYNC         = 0xF550,
+    MSB_ANI_BATCH        = 0xF560,
+    MSB_ANI_COMB         = 0xF570,
+    MSB_ANI_DISABLE      = 0xF580,
+    MSB_MODE_LTAG        = 0xF590,
+    MSB_ANI_EVENTS       = 0xF5A0,
+    MSB_EVENT_SFX        = 0xF5A1,
+    MSB_EVENT_SFX_GRND   = 0xF5A2,
+    MSB_EVENT_TAG        = 0xF5A3,
+    MSB_EVENT_PFX        = 0xF5A4,
+    MSB_EVENT_PFX_STOP   = 0xF5A5,
+    MSB_EVENT_PFX_GRND   = 0xF5A6,
+    MSB_EVENT_SET_MESH   = 0xF5A7,
+    MSB_EVENT_SWAP_MESH  = 0xF5A8,
+    MSB_EVENT_MMSTARTANI = 0xF5A9,
+    MSB_EVENT_CAMTREMOR  = 0xF5AA,
+};
+
+/*
 static const uint16_t MSID_MESHSOFTSKIN = 0xE100;
 static const uint16_t MSID_MESHSOFTSKIN_END = 0xE110;
 
 static const uint16_t MSID_MODELANI = 0xA000;
+*/
+
 static const uint16_t MSID_MAN_HEADER = 0xA020;
 static const uint16_t MSID_MAN_SOURCE = 0xA010;
 static const uint16_t MSID_MAN_ANIEVENTS = 0xA030;
@@ -73,11 +108,11 @@ zCModelAni::zCModelAni()
 
 }
 
-bool zCModelAni::loadMAN(const VDFS::FileIndex &fileIndex, const std::string &fileName)
+bool zCModelAni::loadMAN(const VDFS::FileIndex &index, const std::string &fileName)
 {
     m_ModelAniHeader.version = 0;
 
-    bool ret = parse(fileIndex, fileName, [&](ZenParser &parser) -> bool {
+    bool ret = parse(index, fileName, [&](ZenParser &parser) -> bool {
 
         // Information about a single chunk
         BinaryChunkInfo chunkInfo;
@@ -85,8 +120,7 @@ bool zCModelAni::loadMAN(const VDFS::FileIndex &fileIndex, const std::string &fi
         // Read chunks until we left the virtual binary file or got to the end-chunk
         // Each chunk starts with a header (BinaryChunkInfo) which gives information
         // about what to do and how long the chunk is
-        bool doneReadingChunks = false;
-        while(!doneReadingChunks)
+        while (parser.getSeek() < parser.getFileSize())
         {
             // Read chunk header and calculate position of next chunk
             parser.readStructure(chunkInfo);
@@ -152,9 +186,6 @@ bool zCModelAni::loadMAN(const VDFS::FileIndex &fileIndex, const std::string &fi
                 parser.setSeek(chunkEnd); // Skip chunk
 
             }
-
-            if(parser.getSeek() >= parser.getFileSize())
-                doneReadingChunks = true; // No end-tag in here...
         }
         return true;
     });
@@ -174,6 +205,172 @@ bool zCModelAni::loadMAN(const VDFS::FileIndex &fileIndex, const std::string &fi
     return true;
 }
 
+bool zCModelAni::loadMSB(const VDFS::FileIndex &index, const std::string &fileName)
+{
+    LogInfo() << "load MSB " << fileName;
+
+    return parse(index, fileName, [&](ZenParser &parser) -> bool {
+
+        // Information about a single chunk
+        BinaryChunkInfo chunkInfo;
+
+        // Read chunks until we left the virtual binary file or got to the end-chunk
+        // Each chunk starts with a header (BinaryChunkInfo) which gives information
+        // about what to do and how long the chunk is
+        while (parser.getSeek() < parser.getFileSize())
+        {
+            // Read chunk header and calculate position of next chunk
+            parser.readStructure(chunkInfo);
+
+            //LogInfo() << "chunk " << std::hex << std::setw(4) << std::setfill('0') << chunkInfo.id;
+
+            size_t chunkEnd = parser.getSeek() + chunkInfo.length;
+            switch(chunkInfo.id)
+            {
+            case MSB_MODEL:
+                LogInfo() << "MSB model " << parser.getSeek();
+                break;
+
+            case MSB_ANI:
+                {
+                    std::string name = parser.readLine(true);
+                    uint32_t layer = parser.readBinaryDWord();
+                    std::string next = parser.readLine(true);
+                    float blendIn = parser.readBinaryFloat();
+                    float blendOut = parser.readBinaryFloat();
+                    std::string flags = parser.readLine(true);
+                    std::string asc = parser.readLine(true);
+                    std::string dir = parser.readLine(true);
+                    uint32_t firstFrame = parser.readBinaryDWord();
+                    uint32_t lastFrame = parser.readBinaryDWord();
+                    float maxFps = parser.readBinaryFloat();
+                    float speed = parser.readBinaryFloat();
+                    float collVolScale = parser.readBinaryFloat();
+
+#if 1
+                    LogInfo() << "MSB ani"
+                              << " name: '" << name << "'"
+                              << " layer: " << layer
+                              << " next: " << next
+                              << " blend in: " << blendIn
+                              << " blend out: " << blendOut
+                              << " flags: " << flags
+                              << " dir: " << dir
+                              << " asc: " << asc
+                              << " first frame: " << firstFrame
+                              << " last frame: " << lastFrame
+                              << " max FPS: " << maxFps
+                              << " max speed: " << speed
+                              << " max collision volume scale: " << collVolScale
+                              ;
+#endif
+                }
+                break;
+
+            case MSB_ANI_ALIAS:
+                {
+                    std::string name = parser.readLine(true);
+                    uint32_t layer = parser.readBinaryDWord();
+                    std::string next = parser.readLine(true);
+                    float blendIn = parser.readBinaryFloat();
+                    float blendOut = parser.readBinaryFloat();
+                    std::string flags = parser.readLine(true);
+                    std::string alias = parser.readLine(true);
+                    std::string dir = parser.readLine(true);
+
+                    m_ModelAniHeader.aniName = name;
+                    m_ModelAniHeader.alias = alias;
+                    m_ModelAniHeader.layer = layer;
+                    m_ModelAniHeader.nextAniName = next;
+                    m_ModelAniHeader.blendIn = blendIn;
+                    m_ModelAniHeader.blendOut = blendOut;
+                    m_ModelAniHeader.flags = flags;
+                    m_ModelAniHeader.alias = alias;
+                    m_ModelAniHeader.dir = dir;
+
+#if 1
+                    LogInfo() << "MSB ani alias"
+                              << " name: '" << name << "'"
+                              << " layer: " << layer
+                              << " next: " << next
+                              << " blend in: " << blendIn
+                              << " blend out: " << blendOut
+                              << " flags: " << flags
+                              << " alias: " << alias
+                              << " dir: " << dir
+                              ;
+#endif
+                }
+                break;
+#if 0
+
+            case MSID_MAN_HEADER:
+                m_ModelAniHeader.version = parser.readBinaryWord();
+
+                m_ModelAniHeader.aniName = parser.readLine(true);
+
+                m_ModelAniHeader.layer = parser.readBinaryDWord();
+                m_ModelAniHeader.numFrames = parser.readBinaryDWord();
+                m_ModelAniHeader.numNodes = parser.readBinaryDWord();
+                m_ModelAniHeader.fpsRate = parser.readBinaryFloat();
+                m_ModelAniHeader.fpsRateSource = parser.readBinaryFloat();
+                m_ModelAniHeader.samplePosRangeMin = parser.readBinaryFloat();
+                m_ModelAniHeader.samplePosScaler = parser.readBinaryFloat();
+
+                parser.readBinaryRaw(m_ModelAniHeader.aniBBox, sizeof(m_ModelAniHeader.aniBBox));
+
+                m_ModelAniHeader.nextAniName = parser.readLine(true);
+                break;
+
+            case MSID_MAN_SOURCE:
+                parser.setSeek(chunkEnd); // Skip chunk
+                break;
+
+            case MSID_MAN_ANIEVENTS: {
+                uint32_t numAniEvents = parser.readBinaryDWord();
+                m_AniEvents.resize(numAniEvents);
+
+                for(int i = 0; i < numAniEvents; i++)
+                    m_AniEvents[i].load(parser);
+                }
+                break;
+
+            case MSID_MAN_RAWDATA: {
+                    m_ModelAniHeader.nodeChecksum = parser.readBinaryDWord();
+
+                    m_NodeIndexList.resize(m_ModelAniHeader.numNodes);
+                    parser.readBinaryRaw(m_NodeIndexList.data(), m_NodeIndexList.size() * sizeof(uint32_t));
+
+                    uint32_t numSamples = m_ModelAniHeader.numNodes * m_ModelAniHeader.numFrames;
+                    zTMdl_AniSample* qSamples = new zTMdl_AniSample[numSamples];
+                    m_AniSamples.resize(numSamples);
+
+
+                    parser.readBinaryRaw(qSamples, sizeof(zTMdl_AniSample) * numSamples);
+
+                    for(size_t i = 0; i < numSamples; i++)
+                    {
+                        SampleUnpackTrans(qSamples[i].position, m_AniSamples[i].position, m_ModelAniHeader.samplePosScaler, m_ModelAniHeader.samplePosRangeMin);
+                        SampleUnpackQuat(qSamples[i].rotation, m_AniSamples[i].rotation);
+                    }
+
+                    delete[] qSamples;
+                }
+                break;
+#endif
+            default:
+                parser.setSeek(chunkEnd); // Skip chunk
+            }
+        }
+    });
+}
+
+bool zCModelAni::loadMDS(const VDFS::FileIndex &index, const std::string &fileName)
+{
+    LogError() << "not implemented, skipped loading " << fileName;
+    return false;
+}
+
 bool zCModelAni::parse(const VDFS::FileIndex& fileIndex, const std::string &fileName, std::function<bool (ZenParser &)> fn)
 {
     bool ret = true;
@@ -187,7 +384,7 @@ bool zCModelAni::parse(const VDFS::FileIndex& fileIndex, const std::string &file
 
         if(data.empty())
         {
-            LogError() << "file " << fileName << " does not exist";
+            //LogError() << "file " << fileName << " does not exist";
             return false;
         }
 
@@ -204,22 +401,4 @@ bool zCModelAni::parse(const VDFS::FileIndex& fileIndex, const std::string &file
     }
 
     return ret;
-}
-
-bool zCModelAni::loadMDH(const std::string &fileName)
-{
-    LogError() << "not implemented, skipped loading " << fileName;
-    return false;
-}
-
-bool zCModelAni::loadMSB(const std::string &fileName)
-{
-    LogError() << "not implemented, skipped loading " << fileName;
-    return false;
-}
-
-bool zCModelAni::loadMDS(const std::string &fileName)
-{
-    LogError() << "not implemented, skipped loading " << fileName;
-    return false;
 }
