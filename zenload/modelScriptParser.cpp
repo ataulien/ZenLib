b7f01d58d61b7803f633e65c0c643e4ad4a07126
Alexander Stillich <dr.west@zoho.com>
2017-01-19 20:52:50 +0100

    Started mds parsing

--- zenload/modelScriptParser.cpp
+++ zenload/modelScriptParser.cpp
@@ -1,5 +1,7 @@
 #include <cmath>
 
+#include <utils/logger.h>
+
 #include "modelScriptParser.h"
 #include "zenParser.h"
 
@@ -28,28 +30,28 @@ ModelScriptBinParser::EChunkType ModelScriptBinParser::parse()
     if (m_Zen.getSeek() >= m_Zen.getFileSize())
         return CHUNK_EOF;
 
-    BinaryChunkInfo chunkInfo;
-    m_Zen.readStructure(chunkInfo);
+    BinaryChunkInfo chunk;
+    m_Zen.readStructure(chunk);
 
     // store position after header so that we can skip the chunk
-    size_t chunkEnd = m_Zen.getSeek() + chunkInfo.length;
+    size_t chunk_end = m_Zen.getSeek() + chunk.length;
 
-    switch(chunkInfo.id)
+    switch(chunk.id)
     {
     case CHUNK_ANI:
         readAni();
-        m_Zen.setSeek(chunkEnd);
+        m_Zen.setSeek(chunk_end);
         return CHUNK_ANI;
     case CHUNK_ANI_ALIAS:
         readAlias();
-        m_Zen.setSeek(chunkEnd);
+        m_Zen.setSeek(chunk_end);
         return CHUNK_ANI_ALIAS;
     case CHUNK_EVENT_SFX:
         readSfx();
-        m_Zen.setSeek(chunkEnd);
+        m_Zen.setSeek(chunk_end);
         return CHUNK_EVENT_SFX;
     default:
-        m_Zen.setSeek(chunkEnd);
+        m_Zen.setSeek(chunk_end);
         return parse(); // skip unknown chunk
     }
 
@@ -125,6 +127,22 @@ void ModelScriptBinParser::readSfx()
     m_Sfx.m_Name = m_Zen.readLine(true);
     m_Sfx.m_Value1 = m_Zen.readLine(true);
     m_Sfx.m_Value2 = m_Zen.readLine(true);
+}
+
+ModelScriptTextParser::ModelScriptTextParser(ZenParser &zen)
+    : ModelScriptParser(zen)
+{
+
+}
+
+ModelScriptBinParser::EChunkType ModelScriptTextParser::parse()
+{
+    while (m_Zen.getSeek() < m_Zen.getFileSize())
+    {
+        std::string tok = m_Zen.readString();
+        LogInfo() << "token: " << tok;
+    }
+    return CHUNK_EOF;
 }
 
 } // namespace ZenLoad
