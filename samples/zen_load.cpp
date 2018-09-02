bf969523d2e76bc75f01eb03ff75f6f6b1d7f57e
Andre Taulien <a.taulien@live.de>
2018-09-02 12:13:37 +0200

    Samples: Zen-Load clang format

--- samples/zen_load.cpp
+++ samples/zen_load.cpp
@@ -1,17 +1,17 @@
-#include <vdfs/fileIndex.h>
-#include <zenload/zenParser.h>
-#include <zenload/zCMesh.h>
 #include <iostream>
 #include <utils/export.h>
+#include <vdfs/fileIndex.h>
+#include <zenload/zCMesh.h>
+#include <zenload/zenParser.h>
 
 /**
  * Recursive function to list some data about the zen-file
  */
 void listVobInformation(const std::vector<ZenLoad::zCVobData>& vobs)
 {
-    for(const ZenLoad::zCVobData& v : vobs)
+    for (const ZenLoad::zCVobData& v : vobs)
     {
-        if(!v.visual.empty())
+        if (!v.visual.empty())
         {
             // More information about what a vob stores can be found in the zTypes.h-File
             std::cout << "Vob at " << v.position.toString() << ", Visual: " << v.visual << std::endl;
@@ -27,11 +27,11 @@ void listVobInformation(const std::vector<ZenLoad::zCVobData>& vobs)
  */
 int main(int argc, char** argv)
 {
-    if(argc != 3)
+    if (argc != 3)
     {
-        std::cout   << "Usage: zen_load <vdf-archive> <zen-name>" << std::endl
-                    << "       <vdf-archive>: Path to the vdf-archive to load" << std::endl
-                    << "       <zen-name>: Name of the .ZEN-File inside the given vdf-archive (name only, no folders)" << std::endl;
+        std::cout << "Usage: zen_load <vdf-archive> <zen-name>" << std::endl
+                  << "       <vdf-archive>: Path to the vdf-archive to load" << std::endl
+                  << "       <zen-name>: Name of the .ZEN-File inside the given vdf-archive (name only, no folders)" << std::endl;
         return 0;
     }
 
@@ -40,30 +40,29 @@ int main(int argc, char** argv)
     VDFS::FileIndex vdf;
     vdf.loadVDF(argv[1]);
 
+    // Initialize parser with zenfile from vdf
+    ZenLoad::ZenParser parser(argv[2], vdf);
 
-  // Initialize parser with zenfile from vdf
-  ZenLoad::ZenParser parser(argv[2], vdf);
+    if (parser.getFileSize() == 0)
+    {
+        std::cout << "Error: ZEN-File either not found or empty!" << std::endl;
+        return 0;
+    }
 
-  if(parser.getFileSize() == 0)
-  {
-    std::cout << "Error: ZEN-File either not found or empty!" << std::endl;
-    return 0;
-  }
+    // Since this is a usual level-zen, read the file header
+    // You will most likely allways need to do that
+    parser.readHeader();
 
-  // Since this is a usual level-zen, read the file header
-  // You will most likely allways need to do that
-  parser.readHeader();
+    // Do something with the header, if you want.
+    std::cout << "Author: " << parser.getZenHeader().user << std::endl
+              << "Date: " << parser.getZenHeader().date << std::endl
+              << "Object-count (optional): " << parser.getZenHeader().objectCount << std::endl;
 
-  // Do something with the header, if you want.
-  std::cout << "Author: " << parser.getZenHeader().user << std::endl
-    << "Date: " << parser.getZenHeader().date << std::endl
-    << "Object-count (optional): " << parser.getZenHeader().objectCount << std::endl;
+    // Read the rest of the ZEN-file
+    ZenLoad::oCWorldData world;
+    parser.readWorld(world);
 
-  // Read the rest of the ZEN-file
-  ZenLoad::oCWorldData world;
-  parser.readWorld(world);
-
-  std::cout << "Done reading ZEN!" << std::endl;
+    std::cout << "Done reading ZEN!" << std::endl;
 
     ZenLoad::PackedMesh packedWorldMesh;
     parser.getWorldMesh()->packMesh(packedWorldMesh, 0.01f);
