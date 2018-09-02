053f3e4cefd4597ca754274e84afec040e7a313a
Andre Taulien <a.taulien@live.de>
2018-09-02 12:10:52 +0200

    Samples: Fixed VDFS-Sample not using 'finalize' and thus not working

--- samples/vdf_unpack.cpp
+++ samples/vdf_unpack.cpp
@@ -1,5 +1,5 @@
-#include <vdfs/fileIndex.h>
 #include <iostream>
+#include <vdfs/fileIndex.h>
 
 /**
  * Utility function to write data to disk
@@ -8,15 +8,15 @@ bool writeFile(const std::string& name, const std::string& path, const std::vect
 {
     std::string sep = (path.back() == '/' || path.back() == '\\') ? "" : "/";
     std::string target = path + sep + name;
-    
+
     FILE* f = fopen(target.c_str(), "wb");
-    if(!f)
+    if (!f)
         return false;
-    
+
     fwrite(data.data(), data.size(), 1, f);
-    
+
     fclose(f);
-    
+
     return true;
 }
 
@@ -25,49 +25,55 @@ bool writeFile(const std::string& name, const std::string& path, const std::vect
  */
 int main(int argc, char** argv)
 {
-    if(argc != 3)
+    if (argc != 3)
     {
-        std::cout   << "Usage: vdf_unpack <vdf-archive> <target-path>" << std::endl
-                    << "       <vdf-archive>: Path to the vdf-archive to load" << std::endl
-                    << "       <target-path>: Path to unpack the files to" << std::endl;
+        std::cout << "Usage: vdf_unpack <vdf-archive> <target-path>" << std::endl
+                  << "       <vdf-archive>: Path to the vdf-archive to load" << std::endl
+                  << "       <target-path>: Path to unpack the files to" << std::endl;
         return 0;
     }
 
-    
-    
+
+    // Initialize the system
+    VDFS::FileIndex::initVDFS(argv[0]);
+
+    const std::string archiveToLoad = argv[1];
+    const std::string unpackDestination = argv[2];
+
     // Create file-index to hold information about the files inside the archives
     VDFS::FileIndex vdf;
 
-    vdf.Init(argv[0]);
-    
-    // Load the archive specified by the user. 
+    // Load the archive specified by the user.
     // Note: You can call this method again to load more archives and add more files to the index
-    vdf.loadVDF(argv[1]);
-    
+    vdf.loadVDF(archiveToLoad);
+
+    // Adter every VDF-Archive is loaded, the index has to be built
+    vdf.finalizeLoad();
+
     // Print how many files we loaded
     std::cout << "Loaded " << vdf.getKnownFiles().size() << " files into fileIndex!" << std::endl;
-    
+
     // List all known files
-    for(auto fname : vdf.getKnownFiles())
+    for (auto fileName : vdf.getKnownFiles())
     {
         // Print some information about the file
-        std::cout << fname << std::endl;
-        
+        std::cout << fileName << std::endl;
+
         // Get the data of the file
-        std::vector<uint8_t> data;
-        
+        std::vector<uint8_t> fileBytes;
+
         // You can access a file by their FileInfo-Descriptor or by name.
-        // This would work as well: 
+        // This would work as well:
         //
         //      vdf.getFileData("AddonWorld.zen", data);
         //
         // Filenames are non case sensitive
-        vdf.getFileData(fname, data);
-        
+        vdf.getFileData(fileName, fileBytes);
+
         // Write the file to disk
-        if(!writeFile(fname, argv[2], data))
+        if (!writeFile(fileName, unpackDestination, fileBytes))
             std::cout << " - Failed to write file!" << std::endl;
     }
-    
+
     return 0;
 }
