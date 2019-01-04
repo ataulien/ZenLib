0e8db7834396698ec57ae4ba92bccc3ce004b686
Andre Taulien <a.taulien@live.de>
2019-01-04 14:24:35 +0100

    ZTEX: Add functions to access DDS mipmap data

--- zenload/ztex2dds.h
+++ zenload/ztex2dds.h
@@ -16,6 +16,24 @@ namespace ZenLoad
 	void convertDDSToRGBA8( const std::vector<uint8_t>& ddsData, std::vector<uint8_t>& rgba8Data, int mip=0);
 
 	/**
+	 * @return Total offset (including header) of where the given mip-level starts inside ddsData
+	 */
+	size_t getMipFileOffsetFromDDS(const std::vector<uint8_t>& ddsData, int mip);
+
+	enum class DXTLevel
+	{
+		DXT1,
+		DXT3,
+		DXT5,
+		Unknown,
+	};
+
+	/**
+	 * @return The DXT compression level of the given DDS file
+	 */
+	DXTLevel getDXTLevelFromDDS(const std::vector<uint8_t>& ddsData);
+
+	/**
 	 * @param ddsData Loaded dds
 	 * @return surface info of the given dds
 	 */
